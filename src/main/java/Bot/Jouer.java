89231a1ab0919b3cbe93ed986c42c131d578d033
Tiko <nt506155@etu.unice.fr>
2018-10-25 17:57:22 +0200

    Implement GetDeValeur,GetDeRessources Joueur à 9 lancer Suppression de ressources inutilisé

--- src/main/java/Bot/Jouer.java
+++ src/main/java/Bot/Jouer.java
@@ -5,53 +5,45 @@ import De.*;
 
 public class Jouer {
     private De d1 = new De();
-    private De d2 = new De();
-
-
-    Integer AccOR = 0;
-    Integer AccSOLAIRE = 0 ;
-    Integer AccLUNAIRE = 0;
-    Integer AccVICTOIRE = 0;
-
+    int AccOR = 0;
+    public int NbTour = 0;
+    public Jouer(){
+        d1.InitDe();
+    }
      public final void lancerDe1(){
-        int rand = (int) (Math.random()*6)+1;
+        int rand = (int) (Math.random()*6+1);
         switch(rand){
-            case 1:  AccSOLAIRE =+ 1;
-                System.out.println("Valeur = > " + d1.getDeValeur(1) + " Ressource = > "+ "/////");
-            case 2:  AccOR =+1;
-                System.out.println("Valeur = > " + "////" + " Ressource = > "+ "/////");
-            case 3:  AccOR =+1;
-                System.out.println("Valeur = > " + "////" + " Ressource = > "+ "/////");
-            case 4:  AccOR =+1;
-                System.out.println("Valeur = > " + "////" + " Ressource = > "+ "/////");
-            case 5:  AccOR =+1;
-                System.out.println("Valeur = > " + "////" + " Ressource = > "+ "/////");
-            case 6:  AccOR =+1;
-                System.out.println("Valeur = > " + "////" + " Ressource = > "+ "/////");
+            case 1:   AccOR =+ d1.getDeValeur(1);
+                System.out.println("Tour " + NbTour + " : " + d1.getDeValeur(1) + " Point d'OR");
+                break;
+            case 2:   AccOR =+ d1.getDeValeur(2);
+                System.out.println("Tour " + NbTour + " : " + d1.getDeValeur(2) + " Point d'OR");
+                break;
+            case 3:  AccOR =+ d1.getDeValeur(3);
+                System.out.println("Tour " + NbTour + " : " + d1.getDeValeur(3) + " Point d'OR");
+                break;
+            case 4:  AccOR =+ d1.getDeValeur(4);
+                System.out.println("Tour " + NbTour + " : " + d1.getDeValeur(4) + " Point d'OR");
+                break;
+            case 5:  AccOR =+ d1.getDeValeur(5);
+                System.out.println("Tour " + NbTour + " : " + d1.getDeValeur(5) + " Point d'OR");
+                break;
+            case 6:  AccOR =+ d1.getDeValeur(6);
+                System.out.println("Tour " + NbTour + " : " + d1.getDeValeur(6) + " Point d'OR");
+                break;
         }
 
 
     }
-    void lancerDe2(){
-        int rand = (int) (Math.random()*6)+1;
-        switch(rand){
-            case 1:  AccLUNAIRE =+ 1;
-                System.out.println("Valeur = > " + "////" + " Ressource = > "+ "/////");
-            case 2:  AccVICTOIRE =+2;
-                System.out.println("Valeur = > " + "////" + " Ressource = > "+ "/////");
-            case 3:  AccOR =+1;
-                System.out.println("Valeur = > " + "////" + " Ressource = > "+ "/////");
-            case 4:  AccOR =+1;
-                System.out.println("Valeur = > " + "////" + " Ressource = > "+ "/////");
-            case 5:  AccOR =+1;
-                System.out.println("Valeur = > " + "////" + " Ressource = > "+ "/////");
-            case 6:  AccOR =+1;
-                System.out.println("Valeur = > " + "////" + " Ressource = > "+ "/////");
-        }
-     }
 
-     public final void main(String[] args){
+
+     public static void main(String[] args){
          Jouer jouer1 = new Jouer();
-         Jouer.lancerDe1();}
+         for(int acc = 0 ; acc < 9; acc++)
+         {jouer1.NbTour =+ 1;
+         jouer1.lancerDe1();
+         System.out.println(jouer1.AccOR);
+         }
+    }
 
 }
