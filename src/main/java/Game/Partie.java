db924dcc9c75939c85e26be9c9960380d3053af7
Tiko <nt506155@etu.unice.fr>
2018-10-25 22:03:00 +0200

    Game in 9 rounds(loose if the players have less than 32 point d'or). Mode One player. One dice with golden face.

--- src/main/java/Game/Partie.java
+++ src/main/java/Game/Partie.java
@@ -1,22 +1,24 @@
 package Game;
 import Bot.*;
 import De.*;
-import java.util.Scanner;
 
 public class Partie {
-
-
-    public static void main(String[] args) {
-        System.out.println("------------BIENVENUE SUR DICE-FORGE------------\n\n\n\n");
-
-
-        Jouer partieJ1 = new Jouer();
-
-        partieJ1.lancerDe1();
-
-
-
-    }
+        public static void main(String[] args){
+            System.out.println("------------BIENVENUE SUR DICE-FORGE------------\n");
+            Jouer jouer1 = new Jouer();
+            int accOR = 0;
+            for(int acc = 0 ; acc < 9; acc++) {
+                Face memFd1 = jouer1.lancerDe1();
+                Ressource MemR1 = memFd1.getRes();
+                int memP = memFd1.getValeur();
+                accOR = accOR + memP;
+                System.out.println("Tour : " + (acc + 1) + " Face obtenu : " + memP + " point d'" + MemR1 + " gagné");
+            }
+            System.out.println("Total de point d'" + Ressource.OR + " gagné : " + accOR);
+            if (accOR <= 31)   /* En moyenne on s'attend à avoir 31.5 point */
+                System.out.print("You Loose");
+            else System.out.print("You Win");
+        }
 
 
 }
