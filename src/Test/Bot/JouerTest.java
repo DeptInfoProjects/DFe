bded2ca947de57927f1e0b13b3f01e393d3ce26f
hassanbenab <44161562+hassanbenab@users.noreply.github.com>
2018-11-08 11:28:26 +0100

    backtobasic

---
+++ src/Test/Bot/JouerTest.java
@@ -0,0 +1,19 @@
+
+package Bot;
+
+import De.Ressource;
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class JouerTest {
+    public JouerTest() {
+    }
+
+    @Test
+    public void lancerDe1() {
+        Jouer tjouer = new Jouer();
+        Assert.assertEquals(Ressource.OR, tjouer.lancerDe1().getRes());
+        Assert.assertEquals("This is not Ressource", Ressource.OR, tjouer.lancerDe1().getRes());
+        System.out.println("Unit test1 finished without error");
+    }
+}
