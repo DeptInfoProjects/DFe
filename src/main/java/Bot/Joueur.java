cf70138e1301bf25f82fb690afbc2d8b8e3b9242
aekathens03 <37839249+aekathens03@users.noreply.github.com>
2018-11-22 17:30:42 +0100

    Pom

--- src/main/java/Bot/Joueur.java
+++ src/main/java/Bot/Joueur.java
@@ -1,5 +1,8 @@
 package Bot;
 import De.*;
+import Iles.*;
+
+import java.util.Random;
 
 
 public class Joueur {
@@ -13,6 +16,8 @@ public class Joueur {
         d1.InitDe1();
         d2.InitDe2();
         Sac = new Inventaire();
+
+
 
     }
     public Inventaire getInventaireJoueur(){return this.Sac;}
@@ -33,17 +38,65 @@ public class Joueur {
         else this.d2.setFACE(Achat);
     }
 
-    public String toString() {
+    private int Rand(Integer f) {
+        Random rand = new Random();
+        return rand.nextInt(f);
+    }
+
+    public Face AcheterFace(Joueur J1) {
+        Sanctuaire Jardin = new Sanctuaire();
+        int x = J1.getInventaireJoueur().getNbOR();
+        int rand = Rand(2);
+        if (x == 2) {
+            J1.getInventaireJoueur().setNbOR(x - 2);
+            if (rand == 0) return Jardin.getSanctuaire(2);
+            else {
+                return Jardin.getSanctuaire(1);
+            }
+        }
+
+        if (x == 3) {
+            J1.getInventaireJoueur().setNbOR(x - 3);
+            if (rand == 0) return Jardin.getSanctuaire(3);
+            else return Jardin.getSanctuaire(4);
+
+        }
+
+        if (x == 4 | x == 5) {
+            J1.getInventaireJoueur().setNbOR(x - 4);
+            return Jardin.getSanctuaire(5);
+        }
+
+        if (x == 6 | x == 7) {
+            J1.getInventaireJoueur().setNbOR(x - 6);
+            return Jardin.getSanctuaire(6);
+        }
+
+        if (x == 8 | x == 9 | x == 10 | x == 11) {
+            J1.getInventaireJoueur().setNbOR(x - 8);
+            if (rand == 0) return Jardin.getSanctuaire(7);
+            else return Jardin.getSanctuaire(8);
+
+        }
+        if ( x >= 12 ){
+        J1.getInventaireJoueur().setNbOR(x - 12);
+        return Jardin.getSanctuaire(9);}
+        return J1.d1.compare2Face(d1.FACEMIN(), d2.FACEMIN());
+    }
+
+    public String toString(){
         String vide;
         vide =  "Dé 1 :" + '\t' + '\t'  +'\t' +"Dé 2 : " + '\t' +'\t' +  "Inventaire : " + '\n' +
                 d1.FACE1.getRes() + " " + d1.FACE1.getValeur() + '\t' +'\t' +'\t' +'\t'+ d2.FACE1.getRes() + " " + d2.FACE1.getValeur() + '\t' + "Or       :  "     + Sac.getNbOR()      + '\n' +
                 d1.FACE2.getRes() + " " + d1.FACE2.getValeur() + '\t' +'\t' +'\t' +'\t'+ d2.FACE2.getRes() + " " + d2.FACE2.getValeur() + '\t' + "Solaire  :  "     + Sac.getNbSolaire() + '\n' +
-                d1.FACE3.getRes() + " " + d1.FACE3.getValeur() + '\t' +'\t' +'\t' +'\t'+ d2.FACE3.getRes() + " " + d2.FACE3.getValeur() + '\t' + "Lunaire :  "     + Sac.getNbLunaire() + '\n' +
+                d1.FACE3.getRes() + " " + d1.FACE3.getValeur() + '\t' +'\t' +'\t' +'\t'+ d2.FACE3.getRes() + " " + d2.FACE3.getValeur() + '\t' + "Lunaire :  "      + Sac.getNbLunaire() + '\n' +
                 d1.FACE4.getRes() + " " + d1.FACE4.getValeur() + '\t' +'\t' +'\t' +'\t'+ d2.FACE4.getRes() + " " + d2.FACE4.getValeur() + '\t' + "Victoire :  "     + Sac.getNbVictoire()+ '\n' +
                 d1.FACE5.getRes() + " " + d1.FACE5.getValeur() + '\t' +'\t' +'\t' +'\t'+ d2.FACE5.getRes() + " " + d2.FACE5.getValeur() + '\n' +
                 d1.FACE6.getRes() + " " + d1.FACE6.getValeur() + '\t' +'\t' +'\t' +'\t'+ d2.FACE6.getRes() + " " + d2.FACE6.getValeur() + '\n';
         return vide;
     }
 }
+
+
 
 
